b9a0564b6049b2ab27f934d484cbaa1429941afc
AbdelRahim A. Elmadany <elmadany@gmail.com>
2022-12-06 10:50:16 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,17 +19,13 @@ setup(name='afrolid',
           'tensorboardX==2.5.1',
           'regex',
           'torch==1.11.0',
-          # 'protobuf',
           'sentencepiece==0.1.96',
           'fairseq==0.12.2',
-          # 'pandas',
           'tqdm==4.63.0',
         ],
       entry_points={
             "console_scripts": [
                 "afrolid_cli = afrolid.cli:afrolid_cli",
-                # "turjuman_score = turjuman_cli.score:score_cli",
-                # "afrolid_interactive = afrolid_cli.interactive:interactive_cli",
             ],
         },
       
